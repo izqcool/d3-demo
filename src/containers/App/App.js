365e3d471c17f43a0a15dc9382058a1a21405290
izqcool <584942555@qq.com>
2018-07-10 18:22:48 +0800

    add init

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as styles from './App.module.scss';
+
+
+
+
+export class App extends React.Component {
+  render() {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import {App} from './containers';
+
+import './vendors';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const VDom = (
+    <Router>
+      <Switch>
+        <Route path="/" component={App}/>
+      </Switch>
+    </Router>
+);
+
+ReactDOM.render(VDom, document.getElementById('root'));
 registerServiceWorker();
