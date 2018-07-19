ebb724f847a80daa909152a8005fb623c40893b7
izqcool <584942555@qq.com>
2018-07-19 22:17:29 +0800

    feat(view): add colors change

--- src/containers/HistogramCon/Histogram/Histogram.js
+++ src/containers/HistogramCon/Histogram/Histogram.js
@@ -3,46 +3,88 @@ import * as d3 from 'd3';
 import * as _ from 'lodash';
 import * as styles from './Histogram.module.scss';
 
-export class Histogram {
-  constructor(options) {
-    if(!options ||
-      typeof options.dom === 'undefined' ||
-      typeof options.data === 'undefined'
-    ) {
-      throw Error('dom and data must be set');
+export class Histogram extends React.Component {
+  oldStartColor = null;
+  oldEndColor = null;
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+    this.init = this.init.bind(this);
+    this.drawHistogram = this.drawHistogram.bind(this);
+    this.reSetColor = this.reSetColor.bind(this);
+    this.init();
+  }
+
+
+  // init(options) {
+  //   this.dom = options.dom;
+  //   this.data = options.data;
+  //   this.width = options.width;
+  //   this.height = options.height;
+  //   this.margin = options.margin || {top:0, right:0, bottom:0, left: 0};
+  //   this.isRange = options.isRange || false;
+  //   this.xName = options.xName;
+  //   this.yName = options.yName;
+  //   this.colorStart = options.colorStart;
+  //   this.colorEnd = options.colorEnd;
+  // }
+
+  shouldComponentUpdate(nextProps) {
+    if(nextProps.colorStart !== this.oldStartColor) {
+      this.reSetColor(nextProps.colorStart,nextProps.colorEnd);
+      this.oldStartColor = nextProps.colorStart;
+      this.oldEndColor = nextProps.colorEnd;
     }
 
-    this.init(options);
+    return false;
   }
 
-  init(options) {
-    this.dom = options.dom;
-    this.data = options.data;
-    this.width = options.width;
-    this.height = options.height;
-    this.margin = options.margin || {top:0, right:0, bottom:0, left: 0};
-    this.isRange = options.isRange || false;
-    this.xName = options.xName;
-    this.yName = options.yName;
+  componentDidMount() {
+    this.drawHistogram();
   }
 
-  render() {
-    console.log(this);
+  init() {
+    const {colorStart, colorEnd} = this.props;
+    this.oldStartColor = colorStart;
+    this.oldEndColor = colorEnd;
+  }
+
+
+  reSetColor(start,end) {
+    const {data} = this.props;
+    const newColors = d3.scaleLinear().domain([0, data.length-1]).range([start,end]);
+    const g = d3.select('.canvas_g');
+
+    d3.selectAll('.rect')
+      .each(function (d,i) {
+        d3.select(this).transition()
+          .duration(90)
+          .delay(i*40)
+          .style('fill',newColors(i));
+      });
+  }
+
+  drawHistogram() {
+    let {data, width, height, margin, isRange} = this.props;
+    const {oldStartColor, oldEndColor} = this;
     //clean this canvas
-    d3.select(this.dom).selectAll('*').remove();
+    d3.select(`.${styles.histogram}`).selectAll('*').remove();
 
-    let {data, width, height, margin, isRange} = this;
+    // const colors = d3.scaleOrdinal(['#5A2999','red','#A2F14A']);
+    const colors = d3.scaleLinear().domain([0, data.length-1]).range([oldStartColor,oldEndColor]);
+
     // const min = d3.min(data, ele => ele.y);
     const max = d3.max(data, ele => ele.y);
 
-
     //add canvas(svg)
-    const svg = d3.select(this.dom).append('svg')
-      .attr('class',styles.container)
-      .attr('width',width)
-      .attr('height',height);
+    const svg = d3.select(`.${styles.histogram}`).append('svg')
+    .attr('class',styles.container)
+    .attr('width',width)
+    .attr('height',height);
 
-    const g = svg.append('g');
+    const g = svg.append('g').attr('class','canvas_g');
     g.attr('transform',`translate(${margin.left},${margin.top})`);
 
     //绘图区域真实宽高
@@ -52,15 +94,13 @@ export class Histogram {
     const xScale = d3.scaleBand().range([0,_width]).padding(.4).domain(data.map(d =>d.x1));
     const yScale = d3.scaleLinear().range([_height,0]).domain([0,max]);
 
-
     const xBar = d3.axisBottom(xScale);
     const yBar = d3.axisLeft(yScale);
 
-
     g.append('g')
-      .attr('class',`xAxis ${styles.axis}`)
-      .attr('transform',`translate(0,${_height})`)
-      .call(xBar);
+    .attr('class',`xAxis ${styles.axis}`)
+    .attr('transform',`translate(0,${_height})`)
+    .call(xBar);
 
     g.append('g')
     .attr('class',`yAxis ${styles.axis}`)
@@ -68,58 +108,56 @@ export class Histogram {
 
     if(isRange) {
       const text = g.select('.xAxis')
-        .append('g')
-        .attr('class','tick')
-        .style('opacity', 1)
-        .append('text')
-        .attr('fill','#000')
-        .attr('x',0.5)
-        .attr('y',9)
-        .attr('dy','0.75em')
-        .text(_.last(data.x2))
+      .append('g')
+      .attr('class','tick')
+      .style('opacity', 1)
+      .append('text')
+      .attr('fill','#000')
+      .attr('x',0.5)
+      .attr('y',9)
+      .attr('dy','0.75em')
+      .text(_.last(data.x2))
     }
 
     g.selectAll('.rect')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('class',styles.bar)
-      .attr('class','rect')
-      .attr('x', d=>xScale(d.x1))
-      .attr('y',_height)
-      .attr('width',xScale.bandwidth())
-      .attr('height',0);
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class','rect')
+    .attr('x', d=>xScale(d.x1))
+    .attr('y',_height)
+    .style('fill',(d,i)=>colors(i))
+    .attr('width',xScale.bandwidth())
+    .attr('height',0);
 
 
     //在X轴上等分每个区间
     g.selectAll('.xAxis .tick')
-      .each(function (d,i) {
-        d3.select(this).attr('transform',()=>{
-          let xWidth = 0;
-          if(i===0) {
-            xWidth = 0;
-          }else {
-            xWidth = xScale.paddingOuter() * xScale.step() + (i - 1) * xScale.step() + xScale.bandwidth() + (xScale.paddingInner() * xScale.step())/2;
-          }
-          return `translate(${xWidth},0)`
-        });
+    .each(function (d,i) {
+      d3.select(this).attr('transform',()=>{
+        let xWidth = 0;
+        if(i===0) {
+          xWidth = 0;
+        }else {
+          xWidth = xScale.paddingOuter() * xScale.step() + (i - 1) * xScale.step() + xScale.bandwidth() + (xScale.paddingInner() * xScale.step())/2;
+        }
+        return `translate(${xWidth},0)`
       });
+    });
 
     g.selectAll('rect')
-      .transition()
-      .duration(500)
-      // .attr('height', d=> _height - yScale(d.y))
-      .attr('height', d=> _height - yScale(d.y))
-      .attr('y', d=>yScale(d.y));
+    .transition()
+    .duration(500)
+    // .attr('height', d=> _height - yScale(d.y))
+    .attr('height', d=> _height - yScale(d.y))
+    .attr('y', d=>yScale(d.y));
+  }
 
-
-
-
-
-
-
-
-
+  render() {
+    return (
+        <div className={styles.histogram}>
+        </div>
+    )
   }
 }
 
--- src/containers/HistogramCon/HistogramCon.js
+++ src/containers/HistogramCon/HistogramCon.js
@@ -3,63 +3,218 @@ import * as styles from './HistogramCon.module.scss';
 
 import {Histogram} from './Histogram';
 
+const rangeData = [
+  {
+    x1: 0,
+    x2: 5,
+    y:10
+  },
+  {
+    x1: 5,
+    x2: 8,
+    y:6
+  },
+  {
+    x1: 8,
+    x2: 15,
+    y:27
+  },
+  {
+    x1: 15,
+    x2: 20,
+    y:23
+  },
+  {
+    x1: 20,
+    x2: 25,
+    y:8
+  },
+  {
+    x1: 20,
+    x2: 25,
+    y:13
+  },
+  {
+    x1: 25,
+    x2: 30,
+    y:16
+  },
+  {
+    x1: 30,
+    x2: 35,
+    y:18
+  },
+  {
+    x1: 35,
+    x2: 40,
+    y:30
+  },
+
+  {
+    x1: 40,
+    x2: 46,
+    y:42
+  },
+  {
+    x1: 46,
+    x2: 50,
+    y:34
+  },
+  {
+    x1: 50,
+    x2: 55,
+    y:23
+  },
+  {
+    x1: 55,
+    x2: 60,
+    y:64
+  },
+  {
+    x1: 60,
+    x2: 65,
+    y:53
+  },
+  {
+    x1: 65,
+    x2: 70,
+    y:12
+  },
+  {
+    x1: 70,
+    x2: 75,
+    y:34
+  },
+  {
+    x1: 75,
+    x2: 80,
+    y:45
+  },
+  {
+    x1: 80,
+    x2: 85,
+    y:61
+  },
+];
+
 export class HistogramCon extends React.Component {
 
   constructor(props) {
     super(props);
-    this._render = this._render.bind(this);
+    this.state = {
+      data: rangeData,
+      isRange: true,
+      activeItem: 'GREEN',
+      colorStart: '#F5FEAB',
+      colorEnd: '#2A9543'
+    };
+    this.getBtnItems = this.getBtnItems.bind(this);
+    this.onClickBtn = this.onClickBtn.bind(this);
   }
 
-  componentDidMount() {
-    this._render();
-  }
+  getBtnItems() {
+    const {isRange,activeItem} = this.state;
+    const commonsItem = [
+      {
+        type: 'RED',
+        clickEvent: ()=>this.onClickBtn('RED'),
+        active: false,
+      },
+      {
+        type: 'GREEN',
+        clickEvent: ()=>this.onClickBtn('GREEN'),
+        active: false,
+      },
+      {
+        type: 'WARM',
+        clickEvent: ()=>this.onClickBtn('WARM'),
+        active: false,
+      },
+      {
+        type: 'COOL',
+        clickEvent: ()=>this.onClickBtn('COOL'),
+        active: false,
+      }
+    ];
 
-  _render() {
-    const data = [
+    const notRangeItem = [
       {
-        x1: 0,
-        x2: 5,
-        y:10
+        type: 'X ASC',
+        clickEvent: ()=>this.onClickBtn('X ASC'),
+        active: false,
       },
       {
-        x1: 5,
-        x2: 8,
-        y:6
+        type: 'X DESC',
+        clickEvent: ()=>this.onClickBtn('COOL'),
+        active: false,
       },
       {
-        x1: 8,
-        x2: 15,
-        y:27
+        type: 'Y ASC',
+        clickEvent: ()=>this.onClickBtn('COOL'),
+        active: false,
       },
       {
-        x1: 15,
-        x2: 20,
-        y:23
-      },
-      {
-        x1: 20,
-        x2: 25,
-        y:8
-      },
-      {
-        x1: 20,
-        x2: 25,
-        y:13
-      },
-      {
-        x1: 25,
-        x2: 30,
-        y:16
-      },
-      {
-        x1: 30,
-        x2: 35,
-        y:18
+        type: 'Y DESC',
+        clickEvent: ()=>this.onClickBtn('COOL'),
+        active: false,
       },
     ];
-    const dom = document.querySelector(`.${styles.chartWrapper}`);
-    const histogram = new Histogram({
-      dom,
+
+    const items = [
+        ...(!isRange ? [notRangeItem]: []),
+        ...commonsItem
+    ];
+
+    return items.map((item)=>{
+      return {
+        ...item,
+        active: activeItem === item.type
+      }
+    });
+
+  }
+
+  onClickBtn(type) {
+    switch (type) {
+      case 'RED':
+        this.setState({
+          activeItem: 'RED',
+          colorStart: '#FEDD0A',
+          colorEnd: '#FC3F25'
+        });
+        break;
+
+      case 'GREEN':
+        this.setState({
+          activeItem: 'GREEN',
+          colorStart: '#F5FEAB',
+          colorEnd: '#2A9543'
+        });
+        break;
+
+      case 'WARM':
+        this.setState({
+          activeItem: 'WARM',
+          colorStart: '#831599',
+          colorEnd: '#FC6A32'
+        });
+        break;
+
+      case 'COOL':
+        this.setState({
+          activeItem: 'COOL',
+          colorStart: '#5A2999',
+          colorEnd: '#27E287'
+        });
+        break;
+    }
+
+  }
+
+  render() {
+    const {data, isRange,colorStart,colorEnd} = this.state;
+    const items = this.getBtnItems();
+    const props = {
       data,
       width:800,
       height:300,
@@ -69,18 +224,23 @@ export class HistogramCon extends React.Component {
         top: 30,
         bottom: 30
       },
-      isRange: true
-
-    });
-    histogram.render();
-  }
-
-  render() {
+      isRange,
+      colorStart,
+      colorEnd
+    };
     return (
         <div className={styles.container}>
-          <div className={styles.chartWrapper}>
-
+          <div className={styles.btn}>
+            {
+              items.map((item,i)=>{
+                return <button key={i}
+                               className={`${item.active ? styles.active: ''} ${styles.btn_items}`}
+                               onClick={item.clickEvent}>{item.type}
+                               </button>
+              })
+            }
           </div>
+          <Histogram {...props} />
         </div>
     )
   }
